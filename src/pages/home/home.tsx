307ce96e4c40abad0e3e9e429bc0abae16cb4d0f
yamirkamell <55173620+yamirkamell@users.noreply.github.com>
2022-09-12 19:42:46 -0500

    finish layout (#3)

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,53 +1,31 @@
-/*import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from 'react-redux';
 import axios from "axios";
-import { setInitialState } from "../../redux/home/actions";*/
+import { setInitialState } from "../../redux/home/actions";
 import '../../scss/pages/home.scss'
 import FooterContent from "../../components/footer-content/footer-content";
 import NavBar from "../../components/nav-bar/nav-bar";
 import StartIcon from '../../assets/icons/ic_star.svg';
 import FavoriteIcon from '../../assets/icons/ic-favorite.svg';
-import PortionIcon from '../../assets/icons/ic_portion.svg';
-import TimeIcon from '../../assets/icons/ic_time.svg';
-import ChefIcon from '../../assets/icons/ic_chef.svg';
-import RecipeIcon from '../../assets/food.png';
-
-const details = [
-  {
-    "id": 1,
-    "img": PortionIcon,
-    "alt": 'portion-icon',
-    "title": 'Tamaño de la porción',
-    "subTitle": '4 raciones'
-  },
-  {
-    "id": 2,
-    "img": TimeIcon,
-    "alt": 'time-icon',
-    "title": 'Tiempo de preparación',
-    "subTitle": '10 minutos'
-  },
-  {
-    "id": 3,
-    "img": ChefIcon,
-    "alt": 'portion-icon',
-    "title": 'Dificultad',
-    "subTitle": 'fácil'
-  }
-];
+import { Details, MenuPrincipal } from './type';
+import { InitialState } from "../../redux/home/types";
 
 const Initial = () => {
-  /*const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       return axios
         .get('https://api.spoonacular.com/recipes/complexSearch/?apiKey=1f3ac3bfb1934f81aea82f1fae435dd0')
-        .then((response) => { dispatch(setInitialState(response.data.results)) })
+        .then((response) => {
+          dispatch(setInitialState(response.data.results));
+          setRecipes(response.data.results);
+        })
     }
     fetchRecipes()
   }, [dispatch]);
-*/
+
   return (
     <>
       <NavBar />
@@ -58,21 +36,28 @@ const Initial = () => {
             <span className="span-subtitle"> para todos</span>
           </div>
         </div>
+        <div className='container-menu-principal'>
+          {MenuPrincipal?.map((item) => (
+            <div className='container-item-menu' key={item.id}>
+              <img src={item.img} alt={item.alt} />
+              <span>{item.title}</span>
+            </div>
+          ))}
+        </div>
         <div className='container-new-recipes'>
           <div>
             <span className='span-new-recipes'> Nuevas Recetas</span>
           </div>
           <div className='container-slider-recipe'>
-            {
-              <div className='container-recipe'>
+            {recipes?.map((item: InitialState) => (
+              <div className='container-recipe' key={item.id}>
                 <div className='container-all-recipe'>
-                  <img src={RecipeIcon} alt="recipe-img" className='img-recipe' />
+                  <img src={item.image} alt="recipe-img" className='img-recipe' />
                   <div className='container-title-recipe'>
-                    <span className='span-title-name'> Ojingeo</span>
-                    <span className='span-title-lastname'>Muchim</span>
+                    <span className='span-title-name'> {item.title}</span>
                   </div>
                   <div className='container-details-recipe'>
-                    {details?.map((item) => (
+                    {Details?.map((item) => (
                       <div className='container-details-items' key={item.id}>
                         <img src={item.img} alt={item.alt} />
                         <span className='span-details-title'>{item.title}</span>
@@ -83,19 +68,19 @@ const Initial = () => {
                 </div>
                 <div className='container-basic'>
                   <div>
-                    <img src={StartIcon} alt="start-icon" />
+                    <img src={StartIcon} alt="start-icon" className='icon-basic' />
                     <span className='span-points'>5.0</span>
                   </div>
                   <div>
-                    <img src={FavoriteIcon} alt="favorite-icon" />
+                    <img src={FavoriteIcon} alt="favorite-icon" className='icon-basic' />
                   </div>
                 </div>
               </div>
-            }
+            ))}
           </div>
         </div>
+        <FooterContent />
       </div>
-      <FooterContent />
     </>
   );
 };

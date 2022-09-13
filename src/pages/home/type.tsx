161c1e9f82190c2238c0457cc5dd26675be00b70
yamir Kamell <yamirkamell@gmail.com>
2022-09-12 19:32:07 -0500

    finish layout

---
+++ src/pages/home/type.tsx
@@ -0,0 +1,72 @@
+import PortionIcon from '../../assets/icons/ic_portion.svg';
+import TimeIcon from '../../assets/icons/ic_time.svg';
+import ChefIcon from '../../assets/icons/ic_chef.svg';
+import VegetarianIcon from '../../assets/icons/ic_vegetarian.svg';
+import MainIcon from '../../assets/icons/ic_main.svg';
+import CakeIcon from '../../assets/icons/ic_cake.svg';
+import FastFoodIcon from '../../assets/icons/ic_fast-food.svg';
+import KidsIcon from '../../assets/icons/ic_kids.svg';
+import SoupIcon from '../../assets/icons/ic_soup.svg';
+
+export const Details = [
+  {
+    "id": 1,
+    "img": PortionIcon,
+    "alt": 'portion-icon',
+    "title": 'Tamaño de la porción',
+    "subTitle": '4 raciones'
+  },
+  {
+    "id": 2,
+    "img": TimeIcon,
+    "alt": 'time-icon',
+    "title": 'Tiempo de preparación',
+    "subTitle": '10 minutos'
+  },
+  {
+    "id": 3,
+    "img": ChefIcon,
+    "alt": 'portion-icon',
+    "title": 'Dificultad',
+    "subTitle": 'fácil'
+  }
+];
+
+export const MenuPrincipal = [
+  {
+    "id": 1,
+    "img": VegetarianIcon,
+    "alt": 'vegetarian-icon',
+    "title": 'Vegetarianos',
+  },
+  {
+    "id": 2,
+    "img": MainIcon,
+    "alt": 'main-icon',
+    "title": 'Principales',
+  },
+  {
+    "id": 3,
+    "img": CakeIcon,
+    "alt": 'cake-icon',
+    "title": 'Tortas',
+  },
+  {
+    "id": 4,
+    "img": FastFoodIcon,
+    "alt": 'fastfood-icon',
+    "title": 'Rápida',
+  },
+  {
+    "id": 5,
+    "img": KidsIcon,
+    "alt": 'kid-icon',
+    "title": 'Menú Niños',
+  },
+  {
+    "id": 6,
+    "img": SoupIcon,
+    "alt": 'soup-icon',
+    "title": 'Sopas',
+  }
+];
